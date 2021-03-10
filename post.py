6b334ee28f0b5677fa786ee6daf243ce179a99b2
nekonisi <konishi2175@gmail.com>
2021-03-10 14:04:53 +0900

    一定確率でpostするように修正

--- post.py
+++ post.py
@@ -26,23 +26,23 @@ message_list = {
 # jsonにエンコード
 
 #tmp読み込み
-f = open('tmp', 'r')
+f = open('/home/pi/memento_mori/tmp', 'r')
 tmp = f.read()
 f.close
 
 #乱数生成
-rand = random.randint(0, const.FREQUENCY)
+rand = random.randint(0, int(tmp))
 if tmp == '1' or tmp == '':
-    f = open('tmp', 'w')
+    f = open('/home/pi/memento_mori/tmp', 'w')
     f.write(str(const.FREQUENCY))
     f.close
-elif int(tmp) > int(rand):
-    f = open('tmp', 'w')
-    f.write(str(int(tmp) - 1))
-    f.close
-else:
+elif rand == 1:
     data = json.dumps(message_list)
     response = requests.post(url, headers=headers, data=data)
-    f = open('tmp', 'w')
+    f = open('/home/pi/memento_mori/tmp', 'w')
     f.write(str(const.FREQUENCY))
+    f.close
+else:
+    f = open('/home/pi/memento_mori/tmp', 'w')
+    f.write(str(int(tmp) - 1))
     f.close
