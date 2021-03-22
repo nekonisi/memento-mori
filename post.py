09b5eb4253031ca39152fb6ec54fca9a65da0ddd
nekonisi <konishi2175@gmail.com>
2021-03-22 01:34:13 +0000

    tmpファイルのパスを絶対パス→相対パスに変更

--- post.py
+++ post.py
@@ -4,6 +4,7 @@ import requests
 import json
 import random
 import const
+import os
 
 url = 'https://api.line.me/v2/bot/message/broadcast'
 
@@ -26,23 +27,24 @@ message_list = {
 # jsonにエンコード
 
 #tmp読み込み
-f = open('/home/pi/memento_mori/tmp', 'r')
+tmp_file_path = os.path.dirname(__file__) + '/tmp'
+f = open(tmp_file_path, 'r')
 tmp = f.read()
 f.close
 
 #乱数生成
 rand = random.randint(0, int(tmp))
 if tmp == '1' or tmp == '':
-    f = open('/home/pi/memento_mori/tmp', 'w')
+    f = open(tmp_file_path, 'w')
     f.write(str(const.FREQUENCY))
     f.close
 elif rand == 1:
     data = json.dumps(message_list)
     response = requests.post(url, headers=headers, data=data)
-    f = open('/home/pi/memento_mori/tmp', 'w')
+    f = open(tmp_file_path, 'w')
     f.write(str(const.FREQUENCY))
     f.close
 else:
-    f = open('/home/pi/memento_mori/tmp', 'w')
+    f = open(tmp_file_path, 'w')
     f.write(str(int(tmp) - 1))
     f.close
